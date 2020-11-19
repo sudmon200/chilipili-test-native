80b5f36f14a5ccb53e523d3b887040a8270345b7
sahilmohile15 <sahilmohile15@gmail.com>
2020-11-19 16:50:53 +0530

    Added Montessori Page

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,5 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -6,17 +7,21 @@
 
     <!-- Bootstrap CSS only -->
     <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"> -->
-	<link rel="stylesheet" href="css/imagehover.min.css">
-	<link rel="stylesheet" href="owlcarousel/owl.carousel.min.css">
-	<link rel="stylesheet" href="owlcarousel/owl.theme.default.min.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/imagehover.min.css">
+    <link rel="stylesheet" href="owlcarousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="owlcarousel/owl.theme.default.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <!-- Custom CSS file -->
-    <link rel="stylesheet" href="css/utils.css"> </link>
-    <link rel="stylesheet" href="css/style.css"> </link>
-    <link rel="stylesheet" href="css/responsive.css"> </link>
+    <link rel="stylesheet" href="css/utils.css">
+    </link>
+    <link rel="stylesheet" href="css/style.css">
+    </link>
+    <link rel="stylesheet" href="css/responsive.css">
+    </link>
 </head>
+
 <body>
-<header>
+    <header>
         <div class="brand-logo">
             <a href="#"><img src="media/logo.png" alt="Brand Logo"></a>
         </div>
---
+++ montessori.php
@@ -0,0 +1,27 @@
+<?php
+
+include 'includes/header.php'
+
+?>
+<div class="container" style="height:50px;">
+</div>
+<div class="container header-top m-def">
+    <h3 class="font1">
+        Montessori At Home
+    </h3>
+</div>
+<div class="row">
+    <div class="col-md">
+        <h4>Montessori at Home</h4>
+        <p></p>
+    </div>
+    <div class="col-md"></div>
+</div>
+
+
+
+<?php
+
+include 'includes/footer.php'
+
+?>
