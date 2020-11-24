e514e64b462e36e7554146671e6dcbcd77622223
sahilmohile15 <sahilmohile15@gmail.com>
2020-11-24 11:06:06 +0530

    Added language training section

--- language-training.php
+++ language-training.php
@@ -8,21 +8,146 @@ include 'includes/header.php'
         New Language Training
     </h3>
 </div>
+
+<!-- Header Main Section  -->
 <div class="language-header m-side">
     <h4 class="font1">Advantages of new language training</h4>
     <p class="font3">Learning a new language opens up new vistas, expands the mind, improves social life, builds perspective and breath, enriches the cultural experiences, improves brain functioning and reduces brain related illnesses, builds confidence, helps you meet new people
 </p>
 </div>
-<div class="m-side internal-header">
+
+<!-- Main Div  -->
+
+<div class="m-side internal-header m-def">
     <div class="side1">
-        <h4 class="font1 internal-head">What is montessori at home?</h4>
-        <p class="internal-para font3">Montessori method is widely considered as an effective way to train your child in the early stages of development. Developed over a hundred years ago, it uses specialized learning materials to encourage a child to learn through play and activities. As learning is a continuous process, the child should be having access to the learning aids at all times so that they can engage in learning whenever they desire to do so.Any convenient place in the house can be converted into a Montessori corner where all the learning materials and toys can be stored so that the child can go and engage in learning.</p>
+        <h4 class="font1 internal-head">Individual Training</h4>
+        <p class="internal-para font3">In today’s world, it is not uncommon for people to live and settle in places different from where they were born and brought up. In a muticultural place like india, this involves dealing with local language and culture in most cases. Learning a new language often proves to be a challenge as available methods of learning a new language are often cumbersome. Chilipili New Language training offers the fastest and effective way of picking up a new language at the shortest time possible. Our unique online courses helps in learning the basics of the language, equipping one to read, speak and write a new language with ease in as less as 30 hours. Learn more about our course and enroll into the one most fitting your needs.</p>
     </div>
     <div class="side2">
-        <img src="media/montessori-01.png" alt="Montessori-leaf">
+        <img src="media/montessori-01.png" alt="Language-leaf">
     </div>
 </div>
 
+<div class="internal-body m-def m-side">
+    <div class="side1">
+        <img src="media/montessori-01.png" alt="Montessori-leaf">
+    </div>
+    <div class="side2">
+        <h4 class="font1 internal-head">Institutional Training</h4>
+        <p class="internal-para font3">Chilipili runs a very successful New Language Training module for Educational Institutes and Corporate Houses. This accelerated language course proves to be useful for both professionals as well as students who find themselves in a new city. Our training sessions are conducted at convenient hours and equips the students with basic language skills to navigate their new environment with ease. Professionals like Sales Managers, Civil Service Employees, Business Development Professionals, Entrepreneurs have found this to be an extremely effective tool to make their interactions with the locals more effective and endearing. If you are an educational institute or Corporate house looking out to train your students/employees in a new language within a short period, consider your journey has ended as Chilipili’s concise and compact training will equip them with the required skills quicly and effectively.</p>
+    </div>
+</div>
+
+<!-- Key Feature Icons  -->
+<div class="internal-features m-side">
+    <h3>Key Feature</h3>
+    <div class="first-icon-row">
+        <div class="icon-box">
+            <div class="icon-box-over">
+                <div class="icon">
+                    <img src="media/Icons/ONLINE + OFFLINE.svg" alt="">
+                </div>
+                <div class="icon-name">
+                    <p>Online - Offline</p>
+                </div>
+            </div>
+        </div>
+        <div class="icon-box">
+            <div class="icon-box-over">
+                <div class="icon">
+                    <img src="media/Icons/CONVIENENT SLOTS.svg" alt="">
+                </div>
+                <div class="icon-name">
+                    <p>Convinient Slots</p>
+                </div>
+            </div>
+        </div>
+        <div class="icon-box">
+            <div class="icon-box-over">
+                <div class="icon">
+                    <img src="media/Icons/JOIN ANYTIME.svg" alt="">
+                </div>
+                <div class="icon-name">
+                    <p>Join Anytime</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="second-icon-row">
+    <div class="icon-box">
+            <div class="icon-box-over">
+                <div class="icon">
+                    <img src="media/Icons/ACCELERATED LEARNING.svg" alt="">
+                </div>
+                <div class="icon-name">
+                    <p>Accelarated Learning</p>
+                </div>
+            </div>
+        </div>
+        <div class="icon-box">
+            <div class="icon-box-over">
+                <div class="icon">
+                    <img src="media/Icons/DOWNLOAD CONTENT.svg" alt="">
+                </div>
+                <div class="icon-name">
+                    <p>Downloadable Content</p>
+                </div>
+            </div>
+        </div>
+        <div class="icon-box">
+            <div class="icon-box-over">
+                <div class="icon">
+                    <img src="media/Icons/interactive session.svg" alt="">
+                </div>
+                <div class="icon-name">
+                    <p>Interactive Sections</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="contact-form m-def m-side">
+    <h4 class="font1 m-def text-center">Enquire and Join</h4>
+
+    <div class="contact-form-inside">
+        <div class="row">
+            <div class="col-md">
+                <label for="Full Name">Full Name:</label>
+                <input type="text" name="fname" id="name">
+            </div>
+            <div class="col-md">
+                <label for="Full Name">Email:</label>
+                <input type="email" name="email" id="email">
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-md">
+                <label for="Full Name">Phone Number:</label>
+                <input type="text" name="number" id="number">
+            </div>
+            <div class="col-md">
+                <label for="Full Name">Call us Anytime:</label>
+                <input type="email" name="call" id="call">
+            </div>
+
+        </div>
+        <div class="contact-subject">
+            <div>
+                <label for="Full Name">Subject:</label>
+                <input type="text" name="subject" id="subject">
+            </div>
+        </div>
+        <div class="contact-subject">
+            <div>
+                <label for="Full Name">Query</label>
+                <textarea name="query" id="query" cols="30" rows="3"></textarea>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <?php
