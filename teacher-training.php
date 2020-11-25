8c1e7fc21af1e3bbceb904cc1e2c79f727e7e83f
sahilmohile15 <sahilmohile15@gmail.com>
2020-11-25 13:19:53 +0530

    Updated Teacher Training page

---
+++ teacher-training.php
@@ -0,0 +1,18 @@
+<?php
+
+include 'includes/header.php'
+
+?>
+
+<div class="container header-top m-def">
+    <h3 class="font1">
+        Montessori At Home
+    </h3>
+</div>
+
+
+<?php
+
+include 'includes/footer.php'
+
+?>
