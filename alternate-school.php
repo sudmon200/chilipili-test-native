6e0c2945365c817968dc727f78075fbee2b8c684
sahilmohile15 <sahilmohile15@gmail.com>
2020-11-25 13:18:40 +0530

    Updated alternate school page

---
+++ alternate-school.php
@@ -0,0 +1,125 @@
+<?php
+
+include 'includes/header.php'
+
+?>
+
+<div class="container header-top m-def">
+    <h2 class="font1">
+        Alternate School
+    </h2>
+</div>
+
+<section class="alternate-bg" style="background-image: url(media/final-asset/wave.svg);">
+    <div class="m-side internal-header">
+        <div class="side1">
+            <div class="section-head">
+                <h4 class="font1 internal-head">Relevence of Alternate Learning System</h4>
+            </div>
+            <p class="internal-para font3">Montessori method is widely considered as an effective way to train your child in the early stages of development. Developed over a hundred years ago, it uses specialized learning materials to encourage a child to learn through play and activities. As learning is a continuous process, the child should be having access to the learning aids at all times so that they can engage in learning whenever they desire to do so.Any convenient place in the house can be converted into a Montessori corner where all the learning materials and toys can be stored so that the child can go and engage in learning.</p>
+        </div>
+        <div class="side2">
+            <img src="media/montessori-01.png" alt="Montessori-leaf">
+        </div>
+    </div>
+
+    <div class="alternate-text m-side">
+        <p>However, a child’s development is not confined just to learning concepts but also to many other factors like socialization, team work, interactions, etc. Chilipili school pays keen attention to all the factors which are crucial for a holistic development of a child making it the most unique and effective early education system.</p>
+    </div>
+
+    <div>
+    <img src="media/final-asset/bush-1.svg" class="branch2" alt="branch">
+    </div>
+</section>
+
+<div class="video-box m-side m-def">
+    <div class="video-boxover">
+
+    </div>
+</div>
+<div class="video-box m-side m-def">
+    <div class="video-boxover">
+
+    </div>
+</div>
+<div class="video-box m-side m-def">
+    <div class="video-boxover">
+
+    </div>
+</div>
+
+<div class="internal-features m-side">
+    <div class="section-head">
+        <h3>Key Feature</h3>
+    </div>
+    <div class="first-icon-row">
+        <div class="icon-box">
+            <div class="icon-box-over">
+                <div class="icon">
+                    <img src="media/Icons/PERSONALIZED TEACHING.svg" alt="personalized">
+                </div>
+                <div class="icon-name">
+                    <p>Personalized Teaching</p>
+                </div>
+            </div>
+        </div>
+        <div class="icon-box">
+            <div class="icon-box-over">
+                <div class="icon">
+                    <img src="media/Icons/CONCEPTUAL ICON.svg" alt="conceptual">
+                </div>
+                <div class="icon-name">
+                    <p>Conceptual Learning</p>
+                </div>
+            </div>
+        </div>
+        <div class="icon-box">
+            <div class="icon-box-over">
+                <div class="icon">
+                    <img src="media/Icons/APTITUDE.svg" alt="aptitude">
+                </div>
+                <div class="icon-name">
+                    <p>Aptitude Developement</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="second-icon-row">
+    <div class="icon-box">
+            <div class="icon-box-over">
+                <div class="icon">
+                    <img src="media/Icons/MULTIFACITATE.svg" alt="multi-facetated">
+                </div>
+                <div class="icon-name">
+                    <p>Multi Facetated Curriculum</p>
+                </div>
+            </div>
+        </div>
+        <div class="icon-box">
+            <div class="icon-box-over">
+                <div class="icon">
+                    <img src="media/Icons/colabrative team work.svg" alt="collaborative">
+                </div>
+                <div class="icon-name">
+                    <p>Collaborative Learning Environment</p>
+                </div>
+            </div>
+        </div>
+        <div class="icon-box">
+            <div class="icon-box-over">
+                <div class="icon">
+                    <img src="media/Icons/life skills.svg" alt="life-skill">
+                </div>
+                <div class="icon-name">
+                    <p>Life Skill Developement</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+
+include 'includes/footer.php'
+
+?>
