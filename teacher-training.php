596d7a436d167523b704c6cb7d8954c8099aca94
sahilmohile15 <sahilmohile15@gmail.com>
2020-11-25 14:06:40 +0530

    Updated Teacher Training Page

--- teacher-training.php
+++ teacher-training.php
@@ -5,11 +5,146 @@ include 'includes/header.php'
 ?>
 
 <div class="container header-top m-def">
-    <h3 class="font1">
-        Montessori At Home
-    </h3>
+    <h2 class="font1">
+        Teacher Training
+    </h2>
 </div>
 
+<section class="teacher-bg" style="background-image: url(media/final-asset/wave.svg);">
+    <div class="m-side internal-header">
+        <div class="side1">
+            <div class="section-head">
+                <h4 class="font1 internal-head">Why is Teacher Training Important</h4>
+            </div>
+            <p class="internal-para font3">It is no secret that the teacher plays a crucial role in crafting a child’s future. Along with the parents, the aptitude for learning is cultivated by the attitude of the teacher. Inculcating this love for learning at an early stage is vital to ensure that the child will have a lifelong yearning to learn and excel in life. At Chili Pili, not only are teachers trained to be experts at the subject, but also teaching methodologies, soft skills, planning and assessing so as to be able to tailor teaching to each and every child. We at Chili Pili Teachers Training Program ensure that every teacher comes out not only with very good subject knowledge, but also as an inspirational individual with broad skills to be the guiding light to future generations.</p>
+        </div>
+        <div class="side2">
+            <img src="media/final-asset/Group 6005.svg" alt="Montessori-leaf">
+        </div>
+    </div>
+
+    <div class="alternate-text m-side">
+        <h3>How do we do it</h3>
+        <p>In addition to the requirements of an NTT training course, we train the teachers in multiple other aspects as well – such as use of computers, researching trends and materials, Other pedagogies, hands on experience, bushiness aspects, dealing with parents, as well as how to incorporate innovation and creativity in teaching. We also train them in our unique methodologies, including Chili Pili phonics and our Mathematics program. Teachers are absorbed into Chili Pili schools, language training and other programs as well as for other outside schools and environments.</p>
+    </div>
+
+    <div>
+    <img src="media/final-asset/bush-1.svg" class="branch2" alt="branch">
+    </div>
+</section>
+
+
+
+<div class="internal-features m-side">
+    <div class="section-head">
+        <h3>Features</h3>
+    </div>
+    <div class="first-icon-row">
+        <div class="icon-box">
+            <div class="icon-box-over">
+                <div class="icon">
+                    <img src="media/Icons/NTT certificate.svg" alt="certification">
+                </div>
+                <div class="icon-name">
+                    <p>NTT Certification</p>
+                </div>
+            </div>
+        </div>
+        <div class="icon-box">
+            <div class="icon-box-over">
+                <div class="icon">
+                    <img src="media/Icons/trainer icon.svg" alt="experienced-trainers">
+                </div>
+                <div class="icon-name">
+                    <p>Experienced Trainers</p>
+                </div>
+            </div>
+        </div>
+        <div class="icon-box">
+            <div class="icon-box-over">
+                <div class="icon">
+                    <img src="media/Icons/pedagogies.svg" alt="innovative">
+                </div>
+                <div class="icon-name">
+                    <p>Innovative Pedogogies</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="second-icon-row">
+    <div class="icon-box">
+            <div class="icon-box-over">
+                <div class="icon">
+                    <img src="media/Icons/produvtive icon.svg" alt="prod-tools">
+                </div>
+                <div class="icon-name">
+                    <p>Productivity Tools Training</p>
+                </div>
+            </div>
+        </div>
+        <div class="icon-box">
+            <div class="icon-box-over">
+                <div class="icon">
+                    <img src="media/Icons/soft skill.svg" alt="soft-skills">
+                </div>
+                <div class="icon-name">
+                    <p>Soft Skills Training</p>
+                </div>
+            </div>
+        </div>
+        <div class="icon-box">
+            <div class="icon-box-over">
+                <div class="icon">
+                    <img src="media/Icons/potential energy.svg" alt="creative-potential">
+                </div>
+                <div class="icon-name">
+                    <p>Building Creative Potential</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="contact-form m-def m-side">
+    <h4 class="font1 m-def text-center">Enquire and Join</h4>
+
+    <div class="contact-form-inside">
+        <div class="row">
+            <div class="col-md">
+                <label for="Full Name">Full Name:</label>
+                <input type="text" name="fname" id="name">
+            </div>
+            <div class="col-md">
+                <label for="Full Name">Email:</label>
+                <input type="email" name="email" id="email">
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-md">
+                <label for="Full Name">Phone Number:</label>
+                <input type="text" name="number" id="number">
+            </div>
+            <div class="col-md">
+                <label for="Full Name">Call us Anytime:</label>
+                <input type="email" name="call" id="call">
+            </div>
+
+        </div>
+        <div class="contact-subject">
+            <div>
+                <label for="Full Name">Subject:</label>
+                <input type="text" name="subject" id="subject">
+            </div>
+        </div>
+        <div class="contact-subject">
+            <div>
+                <label for="Full Name">Query</label>
+                <textarea name="query" id="query" cols="30" rows="3"></textarea>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php
 
