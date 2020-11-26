32e0d894650d862f8dba991deefd5fd9fc4ac6bb
Devkant Gorai <devkantgorai1@gmail.com>
2020-11-26 17:42:48 +0530

    added the testimonial slider,fixed the navbar

--- alternate-school.php
+++ alternate-school.php
@@ -5,159 +5,171 @@ include 'includes/header.php'
 ?>
 
 <div class="container header-top m-def">
-    <h2 class="font1">
-        Alternate School
-    </h2>
+	<h2 class="font1">
+		Alternate School
+	</h2>
 </div>
 
 <section class="alternate-bg" style="background-image: url(media/final-asset/Alternate_wave-01.png);">
-    <div class="m-side internal-header">
-        <div class="side1">
-            <div class="section-head">
-                <h4 class="font1 internal-head">Relevence of Alternate Learning System</h4>
-            </div>
-            <p class="internal-para font3">Montessori method is widely considered as an effective way to train your child in the early stages of development. Developed over a hundred years ago, it uses specialized learning materials to encourage a child to learn through play and activities. As learning is a continuous process, the child should be having access to the learning aids at all times so that they can engage in learning whenever they desire to do so.Any convenient place in the house can be converted into a Montessori corner where all the learning materials and toys can be stored so that the child can go and engage in learning.</p>
-        </div>
-        <div class="side2">
-            <img src="media/montessori-01.png" alt="Montessori-leaf">
-        </div>
-    </div>
+	<div class="m-side internal-header">
+		<div class="side1">
+			<div class="section-head">
+				<h4 class="font1 internal-head">Relevence of Alternate Learning System</h4>
+			</div>
+			<p class="internal-para font3">Montessori method is widely considered as an effective way to train your
+				child in the early stages of development. Developed over a hundred years ago, it uses specialized
+				learning materials to encourage a child to learn through play and activities. As learning is a
+				continuous process, the child should be having access to the learning aids at all times so that they can
+				engage in learning whenever they desire to do so.Any convenient place in the house can be converted into
+				a Montessori corner where all the learning materials and toys can be stored so that the child can go and
+				engage in learning.</p>
+		</div>
+		<div class="side2">
+			<img src="media/montessori-01.png" alt="Montessori-leaf">
+		</div>
+	</div>
 
-    <div class="alternate-text m-side">
-        <p>However, a child’s development is not confined just to learning concepts but also to many other factors like socialization, team work, interactions, etc. Chilipili school pays keen attention to all the factors which are crucial for a holistic development of a child making it the most unique and effective early education system.</p>
-    </div>
+	<div class="alternate-text m-side">
+		<p>However, a child’s development is not confined just to learning concepts but also to many other factors like
+			socialization, team work, interactions, etc. Chilipili school pays keen attention to all the factors which
+			are crucial for a holistic development of a child making it the most unique and effective early education
+			system.</p>
+	</div>
 
-    <div>
-    <img src="media/final-asset/bush-1.svg" class="branch2" alt="branch">
-    </div>
-    <div class="video-box m-side m-def">
-    <div class="video-boxover">
-        <iframe src="https://player.vimeo.com/video/460930431" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-    </div>
-</div>
+	<div>
+		<img src="media/final-asset/bush-1.svg" class="branch2" alt="branch">
+	</div>
+	<div class="video-box m-side m-def">
+		<div class="video-boxover">
+			<iframe src="https://player.vimeo.com/video/460930431" width="640" height="564" frameborder="0"
+				allow="autoplay; fullscreen" allowfullscreen></iframe>
+		</div>
+	</div>
 </section>
 
 
 <div class="video-box m-side m-def">
-    <div class="video-boxover">
-        <iframe src="https://player.vimeo.com/video/460930606" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-    </div>
+	<div class="video-boxover">
+		<iframe src="https://player.vimeo.com/video/460930606" width="640" height="564" frameborder="0"
+			allow="autoplay; fullscreen" allowfullscreen></iframe>
+	</div>
 </div>
 <div class="video-box m-side m-def">
-    <div class="video-boxover">
-        <iframe src="https://player.vimeo.com/video/460930299" width="640" height="564" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-    </div>
+	<div class="video-boxover">
+		<iframe src="https://player.vimeo.com/video/460930299" width="640" height="564" frameborder="0"
+			allow="autoplay; fullscreen" allowfullscreen></iframe>
+	</div>
 </div>
 
 <div class="internal-features m-side">
-    <div class="section-head">
-        <h3>Key Feature</h3>
-    </div>
-    <div class="first-icon-row">
-        <div class="icon-box">
-            <div class="icon-box-over">
-                <div class="icon">
-                    <img src="media/Icons/PERSONALIZED TEACHING.svg" alt="personalized">
-                </div>
-                <div class="icon-name">
-                    <p>Personalized Teaching</p>
-                </div>
-            </div>
-        </div>
-        <div class="icon-box">
-            <div class="icon-box-over">
-                <div class="icon">
-                    <img src="media/Icons/CONCEPTUAL ICON.svg" alt="conceptual">
-                </div>
-                <div class="icon-name">
-                    <p>Conceptual Learning</p>
-                </div>
-            </div>
-        </div>
-        <div class="icon-box">
-            <div class="icon-box-over">
-                <div class="icon">
-                    <img src="media/Icons/APTITUDE.svg" alt="aptitude">
-                </div>
-                <div class="icon-name">
-                    <p>Aptitude Developement</p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="second-icon-row">
-    <div class="icon-box">
-            <div class="icon-box-over">
-                <div class="icon">
-                    <img src="media/Icons/MULTIFACITATE.svg" alt="multi-facetated">
-                </div>
-                <div class="icon-name">
-                    <p>Multi Facetated Curriculum</p>
-                </div>
-            </div>
-        </div>
-        <div class="icon-box">
-            <div class="icon-box-over">
-                <div class="icon">
-                    <img src="media/Icons/colabrative team work.svg" alt="collaborative">
-                </div>
-                <div class="icon-name">
-                    <p>Collaborative Learning Environment</p>
-                </div>
-            </div>
-        </div>
-        <div class="icon-box">
-            <div class="icon-box-over">
-                <div class="icon">
-                    <img src="media/Icons/life skills.svg" alt="life-skill">
-                </div>
-                <div class="icon-name">
-                    <p>Life Skill Developement</p>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="section-head">
+		<h3>Key Feature</h3>
+	</div>
+	<div class="first-icon-row">
+		<div class="icon-box">
+			<div class="icon-box-over">
+				<div class="icon">
+					<img src="media/Icons/PERSONALIZED TEACHING.svg" alt="personalized">
+				</div>
+				<div class="icon-name">
+					<p>Personalized Teaching</p>
+				</div>
+			</div>
+		</div>
+		<div class="icon-box">
+			<div class="icon-box-over">
+				<div class="icon">
+					<img src="media/Icons/CONCEPTUAL ICON.svg" alt="conceptual">
+				</div>
+				<div class="icon-name">
+					<p>Conceptual Learning</p>
+				</div>
+			</div>
+		</div>
+		<div class="icon-box">
+			<div class="icon-box-over">
+				<div class="icon">
+					<img src="media/Icons/APTITUDE.svg" alt="aptitude">
+				</div>
+				<div class="icon-name">
+					<p>Aptitude Developement</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="second-icon-row">
+		<div class="icon-box">
+			<div class="icon-box-over">
+				<div class="icon">
+					<img src="media/Icons/MULTIFACITATE.svg" alt="multi-facetated">
+				</div>
+				<div class="icon-name">
+					<p>Multi Facetated Curriculum</p>
+				</div>
+			</div>
+		</div>
+		<div class="icon-box">
+			<div class="icon-box-over">
+				<div class="icon">
+					<img src="media/Icons/colabrative team work.svg" alt="collaborative">
+				</div>
+				<div class="icon-name">
+					<p>Collaborative Learning Environment</p>
+				</div>
+			</div>
+		</div>
+		<div class="icon-box">
+			<div class="icon-box-over">
+				<div class="icon">
+					<img src="media/Icons/life skills.svg" alt="life-skill">
+				</div>
+				<div class="icon-name">
+					<p>Life Skill Developement</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 
 <div class="contact-form m-def m-side">
-    <h4 class="font1 m-def text-center">Enquire and Join</h4>
+	<h4 class="font1 m-def text-center">Enquire and Join</h4>
 
-    <div class="contact-form-inside">
-        <div class="row">
-            <div class="col-md">
-                <label for="Full Name">Full Name:</label>
-                <input type="text" name="fname" id="name">
-            </div>
-            <div class="col-md">
-                <label for="Full Name">Email:</label>
-                <input type="email" name="email" id="email">
-            </div>
+	<div class="contact-form-inside">
+		<div class="row">
+			<div class="col-md">
+				<label for="Full Name">Full Name:</label>
+				<input type="text" name="fname" id="name">
+			</div>
+			<div class="col-md">
+				<label for="Full Name">Email:</label>
+				<input type="email" name="email" id="email">
+			</div>
 
-        </div>
-        <div class="row">
-            <div class="col-md">
-                <label for="Full Name">Phone Number:</label>
-                <input type="text" name="number" id="number">
-            </div>
-            <div class="col-md">
-                <label for="Full Name">Call us Anytime:</label>
-                <input type="email" name="call" id="call">
-            </div>
+		</div>
+		<div class="row">
+			<div class="col-md">
+				<label for="Full Name">Phone Number:</label>
+				<input type="text" name="number" id="number">
+			</div>
+			<div class="col-md">
+				<label for="Full Name">Call us Anytime:</label>
+				<input type="email" name="call" id="call">
+			</div>
 
-        </div>
-        <div class="contact-subject">
-            <div>
-                <label for="Full Name">Subject:</label>
-                <input type="text" name="subject" id="subject">
-            </div>
-        </div>
-        <div class="contact-subject">
-            <div>
-                <label for="Full Name">Query</label>
-                <textarea name="query" id="query" cols="30" rows="3"></textarea>
-            </div>
-        </div>
-    </div>
+		</div>
+		<div class="contact-subject">
+			<div>
+				<label for="Full Name">Subject:</label>
+				<input type="text" name="subject" id="subject">
+			</div>
+		</div>
+		<div class="contact-subject">
+			<div>
+				<label for="Full Name">Query</label>
+				<textarea name="query" id="query" cols="30" rows="3"></textarea>
+			</div>
+		</div>
+	</div>
 </div>
 
 <?php
